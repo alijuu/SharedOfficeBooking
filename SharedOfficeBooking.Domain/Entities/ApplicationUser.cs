eaf4df0fac7ae3bdd65d683951c6bb77db6ebcfe
ademhafe123 <ademsinanovic235@gmail.com>
2025-04-23 13:33:43 +0200

    Updated user class

--- SharedOfficeBooking.Domain/Entities/ApplicationUser.cs
+++ SharedOfficeBooking.Domain/Entities/ApplicationUser.cs
@@ -5,15 +5,11 @@ namespace SharedOfficeBooking.Domain.Entities;
 
 public class ApplicationUser : IdentityUser
 {
-    public Guid Id { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     [MaxLength(255)]
     public string Phone { get; set; } = string.Empty;
     [MaxLength(255)]
     public string Name { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public Role Role { get; set; } = Role.User;
+    public string Role { get; set; } = "User";
 
     public List<Booking> Bookings { get; set; } = new();
 }
